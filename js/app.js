df17966a2a5a87989bc655f8d9c87e6df4431dd8
Amir Hossain <amir508_du@yahoo.com>
2022-02-17 07:50:48 +0600

    error handling added

--- js/app.js
+++ js/app.js
@@ -1,32 +1,45 @@
+// INCOME BLOCK
 function getTotalIncome() {
     //get Total Income
-    if (isNaN(inputIncome.value)) {
-        console.log('error')
+    if (isNaN(inputIncome.value) || inputIncome.value < 0) {
+        alert('input is not a number or negative. please provide valid input');
     } else {
         const totalIncome = parseFloat(document.getElementById('inputIncome').value);
         return totalIncome;
     }
-}
+};
 
+// EXPENSES BLOCK
 function getTotalExpense() {
     //get Total Expense
-    const foodExpense = parseFloat(document.getElementById('inputFood').value);
-    const rentExpense = parseFloat(document.getElementById('inputRent').value);
-    const clothExpense = parseFloat(document.getElementById('inputCloth').value);
-    const totalExpense = foodExpense + rentExpense + clothExpense;
-    return totalExpense;
-}
+    let input = inputFood.value || inputRent.value || inputCloth.value;
+    if (isNaN(input) || input < 0) {
+        alert('input is not a number or negative. please provide valid input');
+    } else {
+        const foodExpense = parseFloat(document.getElementById('inputFood').value);
+        const rentExpense = parseFloat(document.getElementById('inputRent').value);
+        const clothExpense = parseFloat(document.getElementById('inputCloth').value);
+        const totalExpense = foodExpense + rentExpense + clothExpense;
+        return totalExpense;
+    }
+};
 
+// TOTAL EXPENSES & BALANCE BLOCK
 const calculateTotal = document.getElementById('buttonCalc').addEventListener('click', function () {
     //show Total Expense
     let showTotalExpense = document.getElementById('totalExpense');
     showTotalExpense.innerText = getTotalExpense();
     //get total balance
-    const balance = getTotalIncome() - getTotalExpense();
-    const showBalance = document.getElementById('balance');
-    showBalance.innerText = balance;
+    if (showTotalExpense.innerText > getTotalIncome()) {
+        alert('you have exceeded your income');
+    } {
+        const balance = getTotalIncome() - getTotalExpense();
+        const showBalance = document.getElementById('balance');
+        showBalance.innerText = balance;
+    }
 });
 
+// SAVINGS & NET BALANCE BLOCK
 const save = document.getElementById('buttonSave').addEventListener('click', function () {
     //get savings
     const percentSave = document.getElementById('inputSave').value;
@@ -34,8 +47,12 @@ const save = document.getElementById('buttonSave').addEventListener('click', fun
     const totalSaving = getTotalIncome() * parseFloat(percentSave) / 100;
     savingAmount.innerText = totalSaving;
     //get remaining balance
-    const remainingBalance = document.getElementById('remainingBalance');
-    const balance = document.getElementById('balance').innerText;
-    const netBalance = parseFloat(balance) - parseFloat(totalSaving);
-    remainingBalance.innerText = netBalance;
+    if (balance > savingAmount.innerText) {
+        alert('you dont have enough balance');
+    } else {
+        const remainingBalance = document.getElementById('remainingBalance');
+        const balance = document.getElementById('balance').innerText;
+        const netBalance = parseFloat(balance) - parseFloat(totalSaving);
+        remainingBalance.innerText = netBalance;
+    }
 });
